6e4c3b64e277db3c74889988ca7a3cf7ef0be8bb
jonathasobelo <68344507+jonathasobelo@users.noreply.github.com>
2022-05-11 15:50:05 -0300

    criar movimento

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "rigth";
+
+
 function criarBG (){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,5 +22,25 @@ function criarCobrinha(){
     }
 
 }
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakex = snake[0].x;
+    let snakey = snake[0].y;
+
+    if(direction == "rigth") snakex += box;
+    if(direction == "left") snakex -= box;
+    if(direction == "up") snakey -= box;
+    if(direction == "down") snakey += box;
+
+    snake.pop();
+
+    let newHead = {
+        x:snakex,
+        y:snakey
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
